bfba2c90b9ed201320900b30164dab07519e3750
sneg <sneg@example.com>
2016-11-11 10:44:08 +0200

    some work with markup. update html

--- src/components/operationForm/component.js
+++ src/components/operationForm/component.js
@@ -1,8 +1,10 @@
 import template from './operationForm.html';
+import style from './style.less';
 import controller from './controller';
 
 export default {
   template,
+  style,
   controller,
   bindings: {
     accountsData: '<',
